01f91dd335c8d46e39144d40eba8dd1963bc3591
Vitalii Semenov <v.semenov@astrio.net>
2025-01-03 20:23:34 +0500

    install di container

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use DI\Container;
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -10,7 +11,9 @@ http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$container = new Container();
+
+$app = AppFactory::createFromContainer($container);
 
 $app->addErrorMiddleware(
     displayErrorDetails: (bool) getenv('APP_DEBUG'),
