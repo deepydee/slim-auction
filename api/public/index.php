2e9c871e1be09ddb8faea0c73079cc4114ed1d7b
Vitalii Semenov <v.semenov@astrio.net>
2025-01-04 10:42:57 +0500

    extract app bootstrap

--- api/public/index.php
+++ api/public/index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
+use Slim\App;
 use Slim\Factory\AppFactory;
 
 http_response_code(500);
@@ -12,9 +13,7 @@ require __DIR__ . '/../vendor/autoload.php';
 /** @var ContainerInterface $container */
 $container = require __DIR__ . '/../config/container.php';
 
-$app = AppFactory::createFromContainer($container);
-
-(require __DIR__ . '/../config/middleware.php')($app, $container);
-(require __DIR__ . '/../config/routes.php')($app);
+/** @var App $app */
+$app = (require __DIR__ . '/../config/app.php')($container);
 
 $app->run();
