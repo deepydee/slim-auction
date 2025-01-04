5386d0bb7ebd873eb1dc1a15caa352d45a1ca97a
Vitalii Semenov <v.semenov@astrio.net>
2025-01-04 10:33:16 +0500

    add configs autoload

--- api/config/dependencies.php
+++ api/config/dependencies.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-return [
-    'config' => [
-        'debug' => (bool) getenv('APP_DEBUG'),
-    ],
-];
+$files = glob(__DIR__ . '/common/*.php');
+
+$configs = array_map(static fn ($file) => require $file, $files);
+
+return array_merge_recursive(...$configs);
