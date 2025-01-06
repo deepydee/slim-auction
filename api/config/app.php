8de94b7356bbd046ae228d9fd9682bf00931563b
Vitalii Semenov <v.semenov@astrio.net>
2025-01-06 17:53:41 +0500

    install code sniffer

--- api/config/app.php
+++ api/config/app.php
@@ -11,6 +11,6 @@ return static function (ContainerInterface $container): App {
 
     (require __DIR__ . '/../config/middleware.php')($app, $container);
     (require __DIR__ . '/../config/routes.php')($app);
-    
+
     return $app;
 };
