cf7cf0ec788c18196fc9156353a5045845c70f34
Vitalii Semenov <v.semenov@astrio.net>
2025-01-07 18:24:03 +0500

    move http unit tests

--- api/src/Http/Test/Unit/JsonResponseTest.php
+++ api/src/Http/Test/Unit/JsonResponseTest.php
@@ -2,11 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Test\Unit\Http;
+namespace App\Http\Test\Unit;
 
 use App\Http\JsonResponse;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Test;
