ef7ebe94f20b53634d874739ca70eb914bf9669e
Vitalii Semenov <v.semenov@astrio.net>
2025-01-07 20:09:49 +0500

    implement Tokenizer class

--- api/src/Auth/Service/Tokenizer.php
+++ api/src/Auth/Service/Tokenizer.php
@@ -5,9 +5,21 @@ declare(strict_types=1);
 namespace App\Auth\Service;
 
 use App\Auth\Entity\User\Token;
+use DateInterval;
 use DateTimeImmutable;
+use Ramsey\Uuid\Uuid;
 
-interface Tokenizer
+readonly class Tokenizer
 {
-    public function generate(DateTimeImmutable $date): Token;
+    public function __construct(private DateInterval $interval)
+    {
+    }
+
+    public function generate(DateTimeImmutable $date): Token
+    {
+        return new Token(
+            Uuid::uuid4()->toString(),
+            $date->add($this->interval)
+        );
+    }
 }
---
+++ api/src/Auth/Test/Unit/Service/TokenizerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Test\Unit\Service;
+
+use App\Auth\Service\Tokenizer;
+use DateInterval;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Tokenizer::class)]
+final class TokenizerTest extends TestCase
+{
+    #[Test]
+    public function it_can_issue_token(): void
+    {
+        $interval = new DateInterval('PT1H');
+        $date = new \DateTimeImmutable('+1 day');
+
+        $tokenizer = new Tokenizer($interval);
+
+        $token = $tokenizer->generate($date);
+
+        self::assertEquals($date->add($interval), $token->expiresAt());
+    }
+}
