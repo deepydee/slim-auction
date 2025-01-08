186b1ba360ce7c8c533e41d8f4d1b4da25c190a5
Vitalii Semenov <v.semenov@astrio.net>
2025-01-08 10:31:46 +0500

    implement password hasher

---
+++ api/src/Auth/Test/Unit/Service/PasswordHasherTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Test\Unit\Service;
+
+use App\Auth\Service\PasswordHasher;
+use InvalidArgumentException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(PasswordHasher::class)]
+final class PasswordHasherTest extends TestCase
+{
+    #[Test]
+    public function it_can_hash_password(): void
+    {
+        $hasher = new PasswordHasher();
+
+        $hash = $hasher->hash($password = 'new-password');
+
+        self::assertNotEmpty($hash);
+        self::assertNotEquals($password, $hash);
+    }
+
+    #[Test]
+    public function password_cannot_be_empty(): void
+    {
+        $hasher = new PasswordHasher();
+
+        $this->expectException(InvalidArgumentException::class);
+        $hasher->hash('');
+    }
+
+    #[Test]
+    public function hash_can_be_validated(): void
+    {
+        $hasher = new PasswordHasher();
+
+        $hash = $hasher->hash($password = 'new-password');
+
+        self::assertTrue($hasher->validate($password, $hash));
+        self::assertFalse($hasher->validate('wrong-password', $hash));
+    }
+}
