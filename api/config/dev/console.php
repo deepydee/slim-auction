df5b9700b5811b186a8ca01fc269e7569a1f36fe
Vitalii Semenov <v.semenov@astrio.net>
2025-01-12 11:49:45 +0500

    add create command

--- api/config/dev/console.php
+++ api/config/dev/console.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand;
 use Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider;
 use Psr\Container\ContainerInterface;
@@ -10,10 +11,14 @@ return [
     DropCommand::class => static function (ContainerInterface $container): DropCommand {
         return new DropCommand($container->get(EntityManagerProvider::class));
     },
+    CreateCommand::class => static function (ContainerInterface $container): CreateCommand {
+        return new CreateCommand($container->get(EntityManagerProvider::class));
+    },
 
     'config' => [
         'console' => [
             'commands' => [
+                CreateCommand::class,
                 DropCommand::class,
             ],
         ],
