cae38e997f4d3441a5c07a2e176c6cdb9f79c540
Vitalii Semenov <v.semenov@astrio.net>
2025-01-12 20:21:12 +0500

    refactor repository injections

---
+++ api/config/common/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Auth\Entity\User\User;
+use App\Auth\Entity\User\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Psr\Container\ContainerInterface;
+
+return [
+    UserRepository::class => static function (ContainerInterface $container): UserRepository {
+        $em = $container->get(EntityManagerInterface::class);
+        /** @var EntityRepository $repo */
+        $repo = $em->getRepository(User::class);
+
+        return new UserRepository($em, $repo);
+    },
+];
