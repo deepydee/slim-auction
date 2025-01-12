6a684bb7db9d42ce9365e06c1a9860fd85cb554b
Vitalii Semenov <v.semenov@astrio.net>
2025-01-12 11:48:40 +0500

    add config aggregator

--- api/config/dependencies.php
+++ api/config/dependencies.php
@@ -2,28 +2,17 @@
 
 declare(strict_types=1);
 
+use Laminas\ConfigAggregator\ConfigAggregator;
+use Laminas\ConfigAggregator\PhpFileProvider;
+
 $environment = getenv('APP_ENV');
 if ($environment === false) {
     $environment = 'prod';
 }
 
-$common = glob(__DIR__ . '/common/*.php');
-if ($common === false) {
-    $common = [];
-}
+$aggregator = new ConfigAggregator([
+    new PhpFileProvider(__DIR__ . '/common/*.php'),
+    new PhpFileProvider(__DIR__ . '/' . $environment . '/*.php'),
+]);
 
-$other = glob(__DIR__ . '/' . $environment . '/*.php');
-if ($other === false) {
-    $other = [];
-}
-
-$files = array_merge($common, $other);
-
-/**
- * @psalm-suppress MixedReturnStatement
- * @psalm-suppress UnresolvableInclude
- * @psalm-suppress MixedInferredReturnType
- */
-$configs = array_map(static fn (string $file): array => require $file, $files);
-
-return array_replace_recursive(...$configs);
+return $aggregator->getMergedConfig();
