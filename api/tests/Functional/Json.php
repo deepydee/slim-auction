249d5d44957bee241025e1d021bb3947c3d7b376
Vitalii Semenov <v.semenov@astrio.net>
2025-01-19 15:39:30 +0500

    add JSON decode test helper

---
+++ api/tests/Functional/Json.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional;
+
+use JsonException;
+
+final class Json
+{
+    /**
+     * @throws JsonException
+     * @return array<array-key, mixed>
+     */
+    public static function decode(string $data): array
+    {
+        return json_decode($data, true, 512, JSON_THROW_ON_ERROR);
+    }
+}
