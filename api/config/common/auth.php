7f110de83c332df782f67ca7089a1c3cf20de1e1
Vitalii Semenov <v.semenov@astrio.net>
2025-01-19 15:44:26 +0500

    add Tokenizer factory

--- api/config/common/auth.php
+++ api/config/common/auth.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Auth\Entity\User\User;
 use App\Auth\Entity\User\UserRepository;
+use App\Auth\Service\Tokenizer;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Psr\Container\ContainerInterface;
@@ -16,4 +17,20 @@ return [
 
         return new UserRepository($em, $repo);
     },
+
+    Tokenizer::class => static function (ContainerInterface $container): Tokenizer {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @var array{token_ttl: string} $config
+         */
+        $config = $container->get('config')['auth'];
+
+        return new Tokenizer(new DateInterval($config['token_ttl']));
+    },
+
+    'config' => [
+        'auth' => [
+            'token_ttl' => 'PT1H',
+        ],
+    ],
 ];
