95c04f7ae84edd2db9cecc67379414b5c3355521
Vitalii Semenov <v.semenov@astrio.net>
2025-01-22 20:22:29 +0500

    add validation rules

--- api/src/Auth/Command/JoinByEmail/Confirm/Command.php
+++ api/src/Auth/Command/JoinByEmail/Confirm/Command.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Confirm;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 final readonly class Command
 {
     public function __construct(
+        #[Assert\NotBlank]
         public string $token,
     ) {
     }
--- api/src/Auth/Command/JoinByEmail/Request/Command.php
+++ api/src/Auth/Command/JoinByEmail/Request/Command.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 final readonly class Command
 {
     public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Email]
         public string $email,
+        #[Assert\Length(min: 6)]
+        #[Assert\NotBlank]
         public string $password,
     ) {
     }
