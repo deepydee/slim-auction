51ff480c75871c68ba4c90220e918f47648454c2
Vitalii Semenov <v.semenov@astrio.net>
2025-01-24 20:12:04 +0500

    extract welcome component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
-  return (
-    <div className="app">
-      <div className="welcome">
-        <h1>Auction</h1>
-        <p>We will be here soon...</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Welcome/>
+        </div>
+    );
 }
 
 export default App;
