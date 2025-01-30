781dcd44ce1f4c20d81f2ffac762b713d89d4320
Vitalii Semenov <v.semenov@astrio.net>
2025-01-30 21:18:20 +0500

    install eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,25 +1,29 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: {globals: {...globals.browser, ...globals.node, ...globals.jest}}},
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, ...globals.jest },
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     settings: {
       react: {
-        version: "detect"
-      }
+        version: 'detect',
+      },
     },
     rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-      "quotes": ["error", "single", {"avoidEscape": true}]
-    }
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      quotes: ['error', 'single', { avoidEscape: true }],
+    },
   },
   eslintPluginPrettierRecommended,
-];
+]
