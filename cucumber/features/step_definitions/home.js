8495da3a12ec73aa785e3e62339bdf8435d2ae6a
Vitalii Semenov <v.semenov@astrio.net>
2025-01-31 18:20:19 +0500

    add browser page

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -2,8 +2,10 @@ const { Given, When, Then } = require("cucumber");
 
 Given("I am a guest user", function () {});
 
-When("I open home page", function () {
-  return "pending";
+When("I open home page", async function () {
+  this.page = await this.browser.newPage();
+  await this.page.setViewport({ width: 1280, height: 800 });
+  return await this.page.goto("http://gateway:8080");
 });
 
 Then("I see welcome block", function () {
