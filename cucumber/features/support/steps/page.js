b6922201a433972f9723845dc131c10bf39848b9
Vitalii Semenov <v.semenov@astrio.net>
2025-01-31 19:18:20 +0500

    add page timeout settings

--- cucumber/features/support/steps/page.js
+++ cucumber/features/support/steps/page.js
@@ -1,5 +1,9 @@
 const { When } = require('cucumber')
 
-When('I open {string} page', async function (uri) {
-  return await this.page.goto('http://gateway:8080' + uri)
-})
+When(
+  'I open {string} page',
+  { wrapperOptions: { retry: 2 }, timeout: 30000 },
+  async function (uri) {
+    return await this.page.goto('http://gateway:8080' + uri)
+  },
+)
