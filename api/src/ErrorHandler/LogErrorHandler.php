c4cdcfa91b3144a7af8a16db8168b14caf7f105f
Vitalii Semenov <v.semenov@astrio.net>
2025-03-14 19:55:33 +0500

    remove unnecessary logger and constructor

--- api/src/ErrorHandler/LogErrorHandler.php
+++ api/src/ErrorHandler/LogErrorHandler.php
@@ -4,27 +4,13 @@ declare(strict_types=1);
 
 namespace App\ErrorHandler;
 
-use Psr\Http\Message\ResponseFactoryInterface;
-use Psr\Log\LoggerInterface;
 use Slim\Handlers\ErrorHandler;
-use Slim\Interfaces\CallableResolverInterface;
 
 /**
  * @psalm-suppress PropertyNotSetInConstructor
  */
 final class LogErrorHandler extends ErrorHandler
 {
-    protected LoggerInterface $logger;
-
-    public function __construct(
-        CallableResolverInterface $callableResolver,
-        ResponseFactoryInterface $responseFactory,
-        LoggerInterface $logger,
-    ) {
-        parent::__construct($callableResolver, $responseFactory);
-        $this->logger = $logger;
-    }
-
     protected function writeToErrorLog(): void
     {
         //        \Sentry\captureException($this->exception);
